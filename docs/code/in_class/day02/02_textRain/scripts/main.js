a2675b8fd141a853e8f10cee685f92c4ae8951e8
Ezgi Ates <eates@saic.edu>
2020-02-10 13:01:54 -0600

    Update main.js

--- docs/code/in_class/day02/02_textRain/scripts/main.js
+++ docs/code/in_class/day02/02_textRain/scripts/main.js
@@ -78,12 +78,10 @@ function draw() {
     text(fl.char, fl.x, fl.y  );
     fill( 0,210 ,0 ) ;
     text(fl.char, fl.x, fl.y-4);
-    fill( 0,180 ,0 ) ;
-    text(fl.char, fl.x, fl.y-8);
+    
     fill( 0,150,0 ) ; 
     text(fl.char, fl.x, fl.y-12);
-    fill( 0,120,0 ) ;
-    text(fl.char, fl.x, fl.y-16);
+    
     fill( 0,100,0 ) ;
     text(fl.char, fl.x, fl.y-20);
        
